775aa8a20a8bcd6b9bd8463f0294f41f7fa96dfe
Matthew Sanner <gravityskunk@gmail.com>
2023-05-11 16:21:00 -0600

    added incomplete hashtag functionality

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 // Connect to MongoDB
-const dbUrl = 'mongodb://127.0.0.1:27017/blog';
+const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
@@ -17,6 +20,7 @@ db.once("open", () => {
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
+  hashtags: [String],
   date: { type: Date, default: Date.now }
 });
 
@@ -45,9 +49,26 @@ app.get('/admin', (req, res) => {
 
 // Handle the form submission to add a new post
 app.post('/admin', async (req, res) => {
+  const hashtagArray = (content) => {
+    // Match all words starting with '#' and followed by any non-space character
+    const hashtagRegex = /#\w+\b/g;
+
+    // Extract all the hashtags from the post content
+    const hashtags = content.match(hashtagRegex);
+
+    // If no hashtags are found, return an empty array
+    if (!hashtags) {
+      return [];
+    }
+
+    // Remove the '#' symbol from each hashtag and return the result
+    return hashtags.map(hashtag => hashtag.substring(1));
+  };
+
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    hashtags: hashtagArray(req.body.content)
   });
   await post.save();
   res.redirect('/');
@@ -71,6 +92,17 @@ app.get('/posts/:postId', async (req, res) => {
     res.status(500).send('Something went wrong...');
   }
 });
+
+app.get('/hashtags/:hashtag', async (req, res) => {
+  const { hashtag } = req.params;
+
+  // Search for posts with the specified hashtag in the database
+  const posts = await Post.find({ hashtags: hashtag });
+
+  // Render the view page with the list of posts containing the hashtag
+  res.render('hashtag', { hashtag, posts });
+});
+
 
 // Start the server and listen on port 3000
 app.listen(3000, () => console.log('Server listening on port 3000...'));
