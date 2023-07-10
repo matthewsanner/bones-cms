734743714320e0fb8f5f8cea68eda96716636990
Matthew Sanner <gravityskunk@gmail.com>
2023-07-10 15:15:01 -0600

    fixed login failure redirect

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const catchAsync = require('../utilities/catchAsync');
-const users = require('../controllers/users');
-const verifyToken = require('../verifyToken');
+const passport = require("passport");
+const catchAsync = require("../utilities/catchAsync");
+const users = require("../controllers/users");
+const verifyToken = require("../verifyToken");
 
-router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register));
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.get('/verify/:token', verifyToken, users.handleVerifyToken);
+router.get("/verify/:token", verifyToken, users.handleVerifyToken);
 
-router.route('/forgot')
-    .get(users.renderForgot)
-    .post(users.forgot);
+router.route("/forgot").get(users.renderForgot).post(users.forgot);
 
-router.get('/forgot/:token', verifyToken, users.renderChangePassword);
+router.get("/forgot/:token", verifyToken, users.renderChangePassword);
 
-router.post('/newpassword', users.newPassword);
+router.post("/newpassword", users.newPassword);
 
-router.route('/login')
-    .get(users.renderLogin)
-    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/users/login",
+    }),
+    users.login
+  );
 
-router.get('/logout', users.logout);
+router.get("/logout", users.logout);
 
 module.exports = router;
