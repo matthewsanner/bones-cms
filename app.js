070abfd7060ed642fcb5ff1deb1895baf066497d
Matthew Sanner <gravityskunk@gmail.com>
2023-07-10 16:35:38 -0600

    improved redirects and flash messaging

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // Express & Standard Middleware
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const User = require('./models/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const User = require("./models/user");
 const app = express();
 
-let postsRouter = require('./routes/post')
-let usersRouter= require('./routes/user')
+let postsRouter = require("./routes/post");
+let usersRouter = require("./routes/user");
 
 // Connect to MongoDB
 const dbUrl = process.env.DB_URL;
@@ -26,14 +26,14 @@ mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 // Connect App to Middleware
 
 // EJS as View Engine
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 
 // Parse HTTP request body
 
@@ -41,38 +41,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Override Default HTML Method Requests
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 // Serve Static Files from the "public" Directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Authentication/ Authorization
 const secret = process.env.SECRET;
 
 const store = new MongoStore({
-    mongoUrl: dbUrl,
-    secret,
-    touchAfter: 24 * 60 * 60 // doesn't update db if same for amount of seconds
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60, // doesn't update db if same for amount of seconds
 });
 
-store.on('error', function (e) {
-    console.log('Session store error', e)
+store.on("error", function (e) {
+  console.log("Session store error", e);
 });
 
 const sessionConfig = {
-    store: store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        // protect against cookies being extracted by a javascript attack
-        httpOnly: true,
-        // you want this setting for shtml but doesn't work on local server
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
+  store: store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    // protect against cookies being extracted by a javascript attack
+    httpOnly: true,
+    // you want this setting for shtml but doesn't work on local server
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
 
 app.use(session(sessionConfig));
@@ -85,15 +85,16 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.info = req.flash("info");
+  next();
 });
 
 // Routes Declaration
-app.use('/posts', postsRouter);
-app.use('/users', usersRouter);
+app.use("/posts", postsRouter);
+app.use("/users", usersRouter);
 
 // Listen on Port 3000
-app.listen(3000, () => console.log('Server listening on port 3000...'));
+app.listen(3000, () => console.log("Server listening on port 3000..."));
--- controllers/users.js
+++ controllers/users.js
@@ -1,153 +1,161 @@
-const User = require('../models/user');
-const generateVerificationToken = require('../tokenGenerator');
-const transporter = require('../email');
-const tokenGenerator = require('../tokenGenerator');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const User = require("../models/user");
+const generateVerificationToken = require("../tokenGenerator");
+const transporter = require("../email");
+const tokenGenerator = require("../tokenGenerator");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 module.exports.renderRegister = (req, res) => {
-    res.render('register');
-}
+  res.render("register");
+};
 
 module.exports.register = async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-          return res.status(400).json({ error: 'Email is already taken' });
-        }
-        const verificationToken = generateVerificationToken();
-        const verificationLink = `http://localhost:3000/users/verify/${verificationToken}`;
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: 'Email Verification',
-            html: `<p>Please click the following link to verify your email:</p>
-           <a href="${verificationLink}">Verify Email</a>`
-        };
-        const user = new User({ username, email, verificationToken });
-        const registeredUser = await User.register(user, password);
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                res.status(500).send('Failed to send verification email');
-            } else {
-                console.log('Email sent: ' + info.response);
-                res.status(200).send('Verification email sent');
-            }
-        });
-        // login user after registration using passport helper method
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Bones CMS!');
-            res.redirect('/posts');
-        })
-
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/users/register')
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      req.flash("error", "An account is already registered with this email");
+      return res.redirect("/users/register");
     }
-}
+    const verificationToken = generateVerificationToken();
+    const verificationLink = `http://localhost:3000/users/verify/${verificationToken}`;
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Email Verification",
+      html: `<p>Please click the following link to verify your email:</p>
+           <a href="${verificationLink}">Verify Email</a>`,
+    };
+    const user = new User({ username, email, verificationToken });
+    const registeredUser = await User.register(user, password);
+
+    let verificationEmailFlashMessage;
+
+    // Send verification email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        verificationEmailFlashMessage = "Failed to send verification email";
+      } else {
+        console.log("Email sent: " + info.response);
+        verificationEmailFlashMessage = "Verification email sent!";
+      }
+
+      // Login user after registration using passport helper method
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        if (verificationEmailFlashMessage) {
+          req.flash("info", verificationEmailFlashMessage);
+        }
+        req.flash("success", "Welcome to Bones CMS!");
+        res.redirect("/posts");
+      });
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/users/register");
+  }
+};
 
 module.exports.handleVerifyToken = async (req, res) => {
   try {
     const username = req.username;
 
     // Update user's account status as verified
-    await User.updateOne({ username: username }, { $set: { verified: true, verificationToken: "" }});
+    await User.updateOne(
+      { username: username },
+      { $set: { verified: true, verificationToken: "" } }
+    );
 
-    res.send('Email verified successfully!');
+    res.send("Email verified successfully!");
   } catch (error) {
-    console.error('Error updating user:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error updating user:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
 
 module.exports.renderForgot = (req, res) => {
-    res.render('forgot');
-}
-
-module.exports.forgot = async (req, res) => {
-    try {
-        const { email } = req.body;
-        const verificationToken = generateVerificationToken();
-        const verificationLink = `http://localhost:3000/users/forgot/${verificationToken}`;
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: 'Reset your password',
-            html: `<p>Please click the following link to reset your password:</p>
-           <a href="${verificationLink}">Reset Password</a>`
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                res.status(500).send('Failed to send verification email');
-            } else {
-                console.log('Email sent: ' + info.response);
-                res.status(200).send('Verification email sent');
-            }
-        });
-
-        // before this step we need to verify that the account is verified
-        await User.updateOne({ email: email }, { $set: { verificationToken: verificationToken }});
-
-        res.send('Reset password email sent!');
-
-    } catch (e) {
-        // req.flash('error', e.message);
-        res.redirect('register')
-    }
-}
-
-module.exports.renderChangePassword = (req, res) => {
-    res.render('changePassword', { username: req.username });
-}
-
-// FIX THIS
-module.exports.newPassword = async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const user = await User.findOne({ username });
-
-        if (!user) {
-            req.flash('error', 'User not found');
-            return res.redirect('register');
-        }
-
-        // Set the new password using Passport's setPassword method
-        await user.setPassword(password);
-
-        await user.save();
-
-        res.send('Password was reset!');
-        // req.flash('success', 'Password updated successfully');
-        // res.redirect('/users/login');
-    } catch (err) {
-        // req.flash('error', err.message);
-        res.redirect('register');
-    }
+  res.render("forgot");
 };
 
+module.exports.forgot = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const verificationToken = generateVerificationToken();
+    const verificationLink = `http://localhost:3000/users/forgot/${verificationToken}`;
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Reset your password",
+      html: `<p>Please click the following link to reset your password:</p>
+           <a href="${verificationLink}">Reset Password</a>`,
+    };
+
+    // FIX: before this step we need to verify that the account is verified
+    await User.updateOne(
+      { email: email },
+      { $set: { verificationToken: verificationToken } }
+    );
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        throw new Error("Failed to send reset password email");
+      } else {
+        console.log("Email sent: " + info.response);
+        req.flash("success", "Reset password email sent!");
+        res.redirect("/posts");
+      }
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/posts");
+  }
+};
+
+module.exports.renderChangePassword = (req, res) => {
+  res.render("changePassword", { username: req.username });
+};
+
+module.exports.newPassword = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      req.flash("error", "User not found");
+      return res.redirect("register");
+    }
+
+    // Set the new password using Passport's setPassword method
+    await user.setPassword(password);
+
+    await user.save();
+
+    req.flash("success", "Password updated successfully");
+    res.redirect("/users/login");
+  } catch (err) {
+    req.flash("error", err.message);
+    res.redirect("/users/register");
+  }
+};
 
 module.exports.renderLogin = (req, res) => {
-    res.render('login');
-}
+  res.render("login");
+};
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome back!');
-    const redirectUrl = '/posts';
-    res.redirect(redirectUrl);
-}
+  req.flash("success", "Welcome back!");
+  const redirectUrl = "/posts";
+  res.redirect(redirectUrl);
+};
 
 module.exports.logout = (req, res) => {
-    req.logout(err => {
-        if (err) return next(err);
-        req.flash('success', 'Logged out!');
-        const redirectUrl = '/posts';
-        res.redirect(redirectUrl);
-    })
-}
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "Logged out!");
+    const redirectUrl = "/posts";
+    res.redirect(redirectUrl);
+  });
+};
